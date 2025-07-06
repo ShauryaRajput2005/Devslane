dc3f98976d5da40dae8d576a17f1582ce78f0bea
Shikhar Shaurya <shikharshauryarajput@gmail.com>
2025-07-06 13:20:19 +0530

    react-template

---
+++ devslane/assignments50+/vite-react-template/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 Hello Shikhar, your Vite + React app is ready!</h1>
+    </div>
+  );
+}
+
+export default App;
