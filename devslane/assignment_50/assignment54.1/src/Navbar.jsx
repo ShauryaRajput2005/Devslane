956ebb65c43070e239e058830f2e4e32c846a351
Shikhar Shaurya <shikharshauryarajput@gmail.com>
2025-07-06 22:02:15 +0530

    first react assignment

---
+++ devslane/assignment_50/assignment54.1/src/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <div className=" fixed block top:0 w-screen bg-white shadow-md">
+      <div className="max-w-screen-xl mx-auto px-6 py-3 flex items-center">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"
+          alt="Amazon Logo"
+          className="h-10"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
